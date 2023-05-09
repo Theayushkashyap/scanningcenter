858499c6e3fcd50fa9663ec8be73ad767a994e7c
Theayushkashyap <132225092+Theayushkashyap@users.noreply.github.com>
2023-05-09 22:34:12 +0530

    update

--- stafflandingpage/staffpage.php
+++ stafflandingpage/staffpage.php
@@ -4,6 +4,8 @@ session_start();
 if(!isset( $_SESSION['user_name'])){
     header('location;login_form.php');
 }
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 ?>
 
