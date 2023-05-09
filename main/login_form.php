2caaf0592a4ac2b3e95776f44f26ceaf7eb9bba0
Theayushkashyap <132225092+Theayushkashyap@users.noreply.github.com>
2023-05-09 16:55:38 +0530

    working on admin and staff login page

---
+++ main/login_form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form-container">
+        <form action=""method="post">
+            <h3>Login now</h3>
+            <input type="email" name="email" required placeholder="Enter your email" >
+            <input type="password" name="password" required placeholder="Enter your password">
+        <input type="submit" name="submit" value="Login now" class="form-btn">
+        <p>don't have an account ? <a href="register.php">login now</a></p>
+        </form>
+    </div>
+</body>
+</html>
