dbc5ad1ffc028db7c42182d8250ae402989d7e6e
Theayushkashyap <132225092+Theayushkashyap@users.noreply.github.com>
2023-05-12 17:24:15 +0530

    update

--- stafflandingpage/staffpage.php
+++ stafflandingpage/staffpage.php
@@ -8,49 +8,73 @@ header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 ?>
-
 <!DOCTYPE html>
-<!-- Coding By CodingNepal - codingnepalweb.com -->
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Navigation Bar with Search Box</title>
-    <link rel="stylesheet" href="staffpage.css" />
-    <!-- Unicons CSS -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-   <script src="staffpage.js" defer></script>
-  </head>
-  <body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="sstaffpage.css">
+</head>
+
+<body>
     <div class="hero">
-      <nav>
-        <img src=".../stafflandingpage/5087607.png" class="logo">
-      </nav>
+        <nav>
+            <img src="../images/logo.png" class="logo">
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Feature</a></li>
+                <li><a href="#">b</a></li>
+                <li><a href="#">c</a></li>
+                <li><a href="#">d</a></li>
+            </ul>
+            <img src="../images/user.png" class="user pic" onclick="toggleMenu()">
+
+            <div class="sub-menu-wrap" id="subMenu">
+                <div class="sub-menu">
+                    <div class="user-info">
+                        <img src="../images/user.png" >
+                        <h2><span><?php echo $_SESSION['user_name'] ?></span></h2>
+                    </div>
+                    <hr>
+                    <a href="#" class="sub-menu-link">
+                        <img src="../images/profile.png" >
+                        <p>Edit Profile</p>
+                        <span>></span>
+                    </a>
+                    <a href="#" class="sub-menu-link">
+                        <img src="../images/setting.png" >
+                        <p>Settings & Privacy </p>
+                        <span>></span>
+                    </a>
+                    <a href="#" class="sub-menu-link">
+                        <img src="../images/help.png" >
+                        <p>Help & Support</p>
+                        <span>></span>
+                    </a>
+                    <a href="../main/login_form.php" class="sub-menu-link">
+                        <img src="../images/logout.png" >
+                        <p>Logout</p>
+                        <span>></span>
+                    </a>
+                    
+                </div>
+            </div>
+        
+        </nav>
+
 
     </div>
-    <div class="container">
-    <nav class="nav"><span><?php echo $_SESSION['user_name'] ?></span>
-      <i class="uil uil-bars navOpenBtn"></i>
-      <button class="logo">
-      <p>Profile</p>
-      </button>
-     
-      <ul class="nav-links">
-        <i class="uil uil-times navCloseBtn"></i>
-        <button id="profile-button"><p>View profile</p></button>
-        <li><a href="#">New Patient</a></li>
-        <li><a href="#">Patient Record</a></li>
-        <li><a href="#">Patient Bill</a></li>
-        <li><a href="#">Total Bill</a></li>
-        <a href="/scanningcenter/main/login_form.php">Logout</a>
-      </ul>
-      <i class="uil uil-search search-icon" id="searchIcon"></i>
-      <div class="search-box">
-        <i class="uil uil-search search-icon"></i>
-        <input type="text" placeholder="Search here..." />
-      </div>
-      </div>
-    </nav>
-  </body>
+<script>
+    let subMenu = document.getElementById("subMenu");
+
+    function toggleMenu(){
+        subMenu.classList.toggle("open-menu");
+    }
+</script>
+</body>
+
 </html>
