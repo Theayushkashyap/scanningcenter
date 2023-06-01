11744c6c6a9b0eab8f36e74a29d5c803eac4164d
Theayushkashyap <ayushkashyap787@gmail.com>
2023-06-01 13:32:28 +0530

    update

--- database.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM scanning.user_form;
--- index.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navContent = document.querySelector('.nav-content');
-
-hamburger.addEventListener('click', function() {
-  navContent.classList.toggle('show');
-});
