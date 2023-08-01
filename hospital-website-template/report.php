a08780c6072c24d861a13eb86469c5e2670fa0dc
Theayushkashyap <ayushkashyap787@gmail.com>
2023-08-01 23:42:13 +0530

    update

--- hospital-website-template/report.php
+++ hospital-website-template/report.php
@@ -165,14 +165,14 @@ if(isset($_POST['submit'])){
     <!-- Appointment End -->
 
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light mt-5 py-5">
+ <!-- Footer Start -->
+ <div class="container-fluid bg-dark text-light mt-5 py-5">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Get In Touch</h4>
-                    <p class="mb-4">No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor</p>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>123 Street, New York, USA</p>
+                    
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Address: 3rd Main Rd, opposite Krishna Raja Extension, Krishna Raja Extension, Tiptur, Karnataka 572202</p>
                     <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@example.com</p>
                     <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
                 </div>
@@ -190,7 +190,7 @@ if(isset($_POST['submit'])){
                     <div class="d-flex flex-column justify-content-start">
                         <a class="text-light mb-2" href="index.html"><i class="fa fa-angle-right me-2"></i>Home</a>
                         <a class="text-light mb-2" href="about.html"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                        <a class="text-light mb-2" href="appointment.html"><i class="fa fa-angle-right me-2"></i>Book an apointment</a>
+                        <a class="text-light mb-2" href="appointment.php"><i class="fa fa-angle-right me-2"></i>Book an apointment</a>
                         <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Office</a>
                         <a class="text-light" href="contact.html"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
                     </div>
@@ -222,6 +222,7 @@ if(isset($_POST['submit'])){
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <p class="mb-0">Designed by <a class="text-primary" href="https://htmlcodex.com">HTML Codex</a></p>
+                </div>
                 </div>
             </div>
         </div>
