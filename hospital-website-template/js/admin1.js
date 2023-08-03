25b63ee1dbc3ab2c7e95f6a93ff1ce876197003f
Ayush kashyap <132225092+Theayushkashyap@users.noreply.github.com>
2023-08-03 15:57:38 +0530

    update
    pushing data of counter

--- hospital-website-template/js/admin1.js
+++ hospital-website-template/js/admin1.js
@@ -1,17 +1,35 @@
+<script>
 let valueDisplays = document.querySelectorAll(".num");
+let patientValueDisplay = valueDisplays[0]; // Selecting the element for the number of patients
+let staffValueDisplay = valueDisplays[1]; // Selecting the element for the number of staff
 let interval = -10000;
 
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 1;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-let duration = Math.floor(interval / endValue);
-let counter = setInterval(function () {
-startValue += 1;
-valueDisplay.textContent = startValue;
-if (startValue === endValue) {
-    clearInterval(counter);
+// Adjust the duration to increase or decrease the speed of the counters.
+let patientDuration = 10; // Change this value to adjust the speed of patient counter.
+let staffDuration = 50; // Change this value to slow down the staff counter.
+
+let startPatientValue = 1000;
+let endPatientValue = parseInt(patientValueDisplay.getAttribute("data-val"));
+
+let startStaffValue = 1000;
+let endStaffValue = parseInt(staffValueDisplay.getAttribute("data-val"));
+
+function updatePatientCounter() {
+    startPatientValue += Math.ceil((endPatientValue - startPatientValue) / patientDuration);
+    patientValueDisplay.textContent = (startPatientValue < endPatientValue ? startPatientValue : endPatientValue) + "+";
+    if (startPatientValue < endPatientValue) {
+        requestAnimationFrame(updatePatientCounter);
+    }
 }
 
+function updateStaffCounter() {
+    startStaffValue += Math.ceil((endStaffValue - startStaffValue) / staffDuration);
+    staffValueDisplay.textContent = (startStaffValue < endStaffValue ? startStaffValue : endStaffValue);
+    if (startStaffValue < endStaffValue) {
+        requestAnimationFrame(updateStaffCounter);
+    }
+}
 
-}, duration);
-});
+updatePatientCounter();
+updateStaffCounter();
+</script>
